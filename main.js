77046e923e443f90e92fef74011353f035c754e1
akshayyy1 <157446943+akshayyy1@users.noreply.github.com>
2025-04-29 18:19:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `📻  Ziron Music 🎶`, type: ActivityType.Watching }],
+      activities: [{ name: `📻  Ziron Team 🎶`, type: ActivityType.Watching }],
       status: 'idle',
     });
   }, 2000); 
